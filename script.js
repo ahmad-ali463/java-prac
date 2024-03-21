f7e4ea8640382c1d81d6a77266599caf4bd52ea3
Ahmad Ali <spyali463@gmail.com>
2024-03-22 01:37:48 +0500

    ali

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 document.getElementById("gg").onclick=function(){
     let age = +prompt("enter your age")
     
-    let weight = 70
+    let weight = prompt("enter your weight")
    
-    if(age > 18){
+    if(age > 18 && weight <=70){
         alert("you are elder")
         console.log("you are elder")
     }else{
         alert("you  are small")
         console.log("you are small")
     }
+}
+
+document.getElementById("ggg").onclick = function(){
+    let userName = prompt("your username")
+    let password = prompt("your pass")
+
+    if(userName === "Ahmad" && password === "041"){
+        console.log("you are loggined")
+    }
+    else{
+        console.log("your username or password is  incorrect")
+    }
+}
+    
+   
+ 
+document.getElementById("Clear").onclick = function(){
+    document.getElementById("ahmad").innerHTML = "";
 }
